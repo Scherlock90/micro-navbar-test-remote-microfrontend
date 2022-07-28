fdd67a7f4f35890af369d81abfd237a8c6455c33
Sebastian Napora <sna@spyro-soft.com>
2022-07-28 18:05:14 +0200

    add

---
+++ src/root.component.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './index.css'
+
+export default function Root(props) {
+  return (
+    <nav className="nav">
+      <div className="h-16 flex items-center justify-between px-6 bg-primary text-white" id="nav-container">
+        <div className="flex items-center justify-between" id="left-side">
+          Testing !!!!!!!!!!!!!
+        </div>
+      </div>
+    </nav>
+  );
+}
